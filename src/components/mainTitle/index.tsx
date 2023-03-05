963590570667c2625a25f7af9786e372ee551e97
JoaoVictor <joaovictorcarmindo@gmail.com>
2023-03-04 22:52:17 -0300

    edit react reveal

--- src/components/mainTitle/index.tsx
+++ src/components/mainTitle/index.tsx
@@ -15,10 +15,10 @@ export default function MenuCard({
     <>
       <div className="max-[768px]:mt-6 ">
         <Fade bottom>
-          <h1 className=" hidden max-[500px]:block font-medium text-5xl text-white max-[768px]:text-center">
+          <h1 className=" hidden max-[500px]:block font-medium text-4xl text-white max-[768px]:text-center">
             {titleResponsive}
           </h1>
-          <h1 className=" max-[500px]:hidden font-medium text-5xl text-white max-[768px]:text-center">
+          <h1 className=" max-[500px]:hidden font-medium text-4xl text-white max-[768px]:text-center">
             {title}
           </h1>
           <p className="font-medium text-2xl text-slate-100 py-3 max-[768px]:text-center">
